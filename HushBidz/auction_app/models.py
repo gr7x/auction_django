b8f7c67c4dec51bdfcd00429890244e08ec93601
Richard Reid <r.gavin.reid@aggiemail.usu.edu>
2019-10-24 11:19:57 -0600

    small changes

--- HushBidz/auction_app/models.py
+++ HushBidz/auction_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+
+#class Auction(models.Model):
+#    name = models.CharField(max_length=50)
+#    auction_type = models.BooleanField()
+#    description = models.TextField(max_length=300)
+   # admin=models.ForeignKey(Something... , on_delete=models.CASCADE)
+#    start_time=models.DateTimeField('start_time')
+#    end_time=models.DateTimeField('end_time')
+#    auction_url=
