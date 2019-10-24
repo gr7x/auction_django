33d1744ad5ace6e4fe8a2cb29ca98ec05ec40dde
Laryssa Revelli <lreesaguymon@gmail.com>
2019-10-23 21:07:41 -0600

    adding manage auction page skeleton

--- HushBidz/auction_app/views.py
+++ HushBidz/auction_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Auction, Item, Bid, User
+from .forms import DateForm
 
 # Create your views here.
+
+
+def manage_auction(request):	
+    #auctions = Auction.objects.order_by('-pub_date')
+    #context = {
+    #    'auctions': auctions,
+    #    #deadline'now': time.strftime('%c'), 
+    #}
+    return render(request, 'auction_app/manage_auction.html')#, context)
