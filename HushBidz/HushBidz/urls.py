bfc00c836200cb492df4150f2f26ba28631ad688
lreesa <54558114+lreesa@users.noreply.github.com>
2019-10-23 21:23:48 -0600

    Corrected URL path in HushBidz

--- HushBidz/HushBidz/urls.py
+++ HushBidz/HushBidz/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('auction_app/', include('auction_app.urls'))
-    path('admin/', admin.site.urls),
+    path('auction_app/', include('auction_app.urls')),
+    path(r'^admin/', admin.site.urls),
 ]
