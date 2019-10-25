f477b0996b8270bbbbd30afa740ae3f62e159b45
gr7x <r.gavin.reid@aggiemail.usu.edu>
2019-10-25 16:20:19 -0600

    default code

--- HushBidz/auction_app/views.py
+++ HushBidz/auction_app/views.py
@@ -12,3 +12,8 @@ def manage_auction(request):
     #    #deadline'now': time.strftime('%c'), 
     #}
     return render(request, 'auction_app/manage_auction.html')#, context)
+
+
+#def create_auction(request):
+    #template = loader.get_template('setup_auction.html')
+#    return render(request,'setup_auction.html')
