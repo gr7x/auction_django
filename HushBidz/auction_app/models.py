e4dd2e5d99b4db407a03790d52c7b20011b21e53
gr7x <r.gavin.reid@aggiemail.usu.edu>
2019-10-28 19:31:12 -0600

    added items model

--- HushBidz/auction_app/models.py
+++ HushBidz/auction_app/models.py
@@ -10,11 +10,18 @@ class User(models.Model):
 
 
 
-#class Auction(models.Model):
-#    name = models.CharField(max_length=50)
-#    auction_type = models.BooleanField()
-#    description = models.TextField(max_length=300)
+class Auction(models.Model):
+    name = models.CharField(max_length=256)
+    auction_type = models.BooleanField()
+    description = models.TextField(max_length=256)
    # admin=models.ForeignKey(Something... , on_delete=models.CASCADE)
-#    start_time=models.DateTimeField('start_time')
-#    end_time=models.DateTimeField('end_time')
+    start_time=models.DateTimeField('start_time')
+    end_time=models.DateTimeField('end_time')
 #    auction_url=
+
+class Items(models.Model):
+    name = models.CharField(max_length=256)  
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(max_length=256)
+    
+   # needs upload image added
