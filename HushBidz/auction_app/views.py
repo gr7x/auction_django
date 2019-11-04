0555d579b018276f16b0b00de0598efcca14ce9e
gr7x <r.gavin.reid@aggiemail.usu.edu>
2019-11-04 15:58:00 -0700

    created auction

--- HushBidz/auction_app/views.py
+++ HushBidz/auction_app/views.py
@@ -34,6 +34,6 @@ def manage_auction(request):
     return render(request, 'auction_app/manage_auction.html', context)
 
 
-#def create_auction(request):
-    #template = loader.get_template('setup_auction.html')
-#    return render(request,'setup_auction.html')
+def create_auction(request):
+    template = loader.get_template('setup_auction.html')
+    return render(request,'setup_auction.html')
