052712c388375020a63ce469f1bc1d327d2400bd
gr7x <r.gavin.reid@aggiemail.usu.edu>
2019-11-04 15:57:18 -0700

    registered apps

--- HushBidz/auction_app/admin.py
+++ HushBidz/auction_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import User
-
-admin.site.register(User)
-
+from .models import Auction
+from .models import Items
 # Register your models here.
 
-#admin.site.register(Auction)
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Items)
