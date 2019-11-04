a8e262edc1627203c3aa763287b1674cd37c3f3d
gr7x <r.gavin.reid@aggiemail.usu.edu>
2019-11-04 15:58:25 -0700

    added items

--- HushBidz/auction_app/views.py
+++ HushBidz/auction_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from .models import Auction, Item, User
+from .models import Auction, Items, User
 #from .forms import DateForm
 
 # Create your views here.
