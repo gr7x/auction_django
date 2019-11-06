f848bb31989495f0ffb3ea5428c7b819fdac191d
Laryssa Revelli <lreesaguymon@gmail.com>
2019-11-06 14:46:23 -0700

    Fixed bug in manage auctions

--- HushBidz/auction_app/urls.py
+++ HushBidz/auction_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register$', views.register, name='register'),
     url(r'^create_auction$', views.create_auction, name='setup_auction'),
-#    path('manage_auction', views.manage_auction, name='manage_auction'),
+    url(r'^manage_auction$', views.manage_auction, name='manage_auction'),
 ]
