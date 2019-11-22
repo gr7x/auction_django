45b448b246c6322dda65419330e65d14b58c8636
Laryssa Revelli <lreesaguymon@gmail.com>
2019-11-21 19:52:22 -0700

    added widgets

--- HushBidz/auction_app/forms.py
+++ HushBidz/auction_app/forms.py
@@ -8,16 +8,31 @@ class AddAuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
         fields = ('name', 'description', 'start_time', 'end_time')
-
+        widgets = {
+            'name': forms.TextInput(attrs={'required': True}),
+            'description': forms.TextInput(attrs={'required': True}),
+            'start_time': forms.DateTimeInput(attrs={'required': True}),
+            'end_time': forms.DateTimeInput(attrs={'required': True}),
+        }
 
 
 class AddItemForm(forms.ModelForm):
    class Meta:
         model = Items
         fields = ('name', 'price', 'description', 'image')
+        widgets = {
+            'name': forms.TextInput(attrs={'required': True}),
+            'price': forms.TextInput(attrs={'required': True}),
+            'image': forms.FileInput(attrs={'required': False}),
+        }
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Items
         fields = ('highest_bidder', 'price')
+        widgets = {
+            'highest_bidder': forms.TextInput(attrs={'required': True}),
+            'price': forms.TextInput(attrs={'required': True}),
+
+        }
